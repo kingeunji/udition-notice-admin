67ca368a8d52183f675fb470c2032acfa7132df0
DasolJong <dasoljong92@gmail.com>
2020-03-25 14:16:59 +0900

    초기세팅 오류 해결

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Main />
   </div>
 </template>
+
+<script>
+import Main from "./views/notice/editor/Main";
+
+export default {
+  name: "App",
+  components: {
+    Main
+  }
+};
+</script>
 
 <style lang="scss">
 * {
